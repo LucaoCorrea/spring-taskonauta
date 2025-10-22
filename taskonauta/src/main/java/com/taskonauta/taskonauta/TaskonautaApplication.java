882de196eb535f2adfc5a913898661ccf47ada0e
LucaoCorrea <lucascorrea1505@gmail.com>
2025-10-22 16:03:52 -0300

    setup

---
+++ taskonauta/src/main/java/com/taskonauta/taskonauta/TaskonautaApplication.java
@@ -0,0 +1,13 @@
+package com.taskonauta.taskonauta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskonautaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskonautaApplication.class, args);
+	}
+
+}
